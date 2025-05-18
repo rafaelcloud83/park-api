4f3dd0ffd825ce6c9954f6df940579536b8796dc
rafaelcloud83 <rafaelcloud83@gmail.com>
2025-05-17 21:46:09 -0300

    feat:implementado método para buscar usuário pelo id

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 <p align="center">
   <img src="https://img.shields.io/static/v1?label=java&message=language&color=blue&style=for-the-badge&logo=JAVA"/>  
   <img src="https://img.shields.io/static/v1?label=spring&message=framework&color=blue&style=for-the-badge&logo=SPRING"/>   
-  <img src="https://img.shields.io/static/v1?label=STATUS&message=em desenvolvimento&color=yellow&style=for-the-badge"/>
+  <img src="https://img.shields.io/static/v1?label=STATUS&message=DESENVOLVIMENTO&color=red&style=for-the-badge"/>
 </p>
 
--- src/main/java/edu/rafael/park_api/web/controller/UsuarioController.java
+++ src/main/java/edu/rafael/park_api/web/controller/UsuarioController.java
@@ -5,10 +5,7 @@ import edu.rafael.park_api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/usuarios")
@@ -21,5 +18,11 @@ public class UsuarioController {
     public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
         Usuario user = usuarioService.salvar(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
