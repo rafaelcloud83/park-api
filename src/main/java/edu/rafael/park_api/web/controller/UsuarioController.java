896810ef0baf021f8a7e20ee0f05770b9e020ce4
rafaelcloud83 <rafaelcloud83@gmail.com>
2025-05-18 00:30:13 -0300

    refactor:adicionado biblioteca Bean Validation

--- src/main/java/edu/rafael/park_api/web/controller/UsuarioController.java
+++ src/main/java/edu/rafael/park_api/web/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import edu.rafael.park_api.web.dto.UsuarioCreateDto;
 import edu.rafael.park_api.web.dto.UsuarioResponseDto;
 import edu.rafael.park_api.web.dto.UsuarioSenhaDto;
 import edu.rafael.park_api.web.dto.mapper.UsuarioMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +22,7 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping()
-    public ResponseEntity<UsuarioResponseDto> create(@RequestBody UsuarioCreateDto createDto) {
+    public ResponseEntity<UsuarioResponseDto> create(@Valid @RequestBody UsuarioCreateDto createDto) {
         Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
@@ -33,7 +34,7 @@ public class UsuarioController {
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody UsuarioSenhaDto dto) {
+    public ResponseEntity<Void> updatePassword(@PathVariable Long id, @Valid @RequestBody UsuarioSenhaDto dto) {
         Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
--- src/main/java/edu/rafael/park_api/web/dto/UsuarioCreateDto.java
+++ src/main/java/edu/rafael/park_api/web/dto/UsuarioCreateDto.java
@@ -1,5 +1,8 @@
 package edu.rafael.park_api.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -8,6 +11,10 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 public class UsuarioCreateDto {
+    @NotBlank
+    @Email(message = "Formato de e-mail inválido", regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
     private String username;
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String password;
 }
