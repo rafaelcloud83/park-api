c84726e4b095eea533604b268b291d1a3a4b0b43
rafaelcloud83 <rafaelcloud83@gmail.com>
2025-05-19 21:02:40 -0300

    refactor:implementado auditoria no sistema

--- src/main/java/edu/rafael/park_api/entity/Usuario.java
+++ src/main/java/edu/rafael/park_api/entity/Usuario.java
@@ -2,6 +2,11 @@ package edu.rafael.park_api.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -13,6 +18,7 @@ import java.util.Objects;
 @AllArgsConstructor
 @Entity
 @Table(name = "usuarios")
+@EntityListeners(AuditingEntityListener.class)
 public class Usuario implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,12 +32,16 @@ public class Usuario implements Serializable {
     @Column(name = "role", nullable = false, length = 25)
     private Role role = Role.ROLE_CLIENTE;
 
+    @CreatedDate
     @Column(name = "data_criacao")
     private LocalDateTime dataCriacao;
+    @LastModifiedDate
     @Column(name = "data_modificacao")
     private LocalDateTime dataModificacao;
+    @CreatedBy
     @Column(name = "criado_por")
     private String criadoPor;
+    @LastModifiedBy
     @Column(name = "modificado_por")
     private String modificadoPor;
 
