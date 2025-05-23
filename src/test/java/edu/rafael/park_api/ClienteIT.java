48b8da1c05e6a887cccca205a6f1d4e3ceb18965
rafaelcloud83 <rafaelcloud83@gmail.com>
2025-05-22 21:20:03 -0300

    test:implementado teste de ponta a ponta no método criar cliente

---
+++ src/test/java/edu/rafael/park_api/ClienteIT.java
@@ -0,0 +1,119 @@
+package edu.rafael.park_api;
+
+import edu.rafael.park_api.web.dto.ClienteCreateDto;
+import edu.rafael.park_api.web.dto.ClienteResponseDto;
+import edu.rafael.park_api.web.exception.ErrorMessage;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/clientes/clientes-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/clientes/clientes-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class ClienteIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    public void createCliente_ComDadosValidos_RetornarClienteComStatus201(){
+        ClienteResponseDto responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "jose@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("José Castro", "68827097007"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(ClienteResponseDto.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getId()).isNotNull();
+        Assertions.assertThat(responseBody.getNome()).isEqualTo("José Castro");
+        Assertions.assertThat(responseBody.getCpf()).isEqualTo("68827097007");
+    }
+
+    @Test
+    public void createCliente_ComCpfCadastrado_RetornarErrorMessageComStatus409(){
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "jose@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("José Castro", "81315448009"))
+                .exchange()
+                .expectStatus().isEqualTo(409)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
+    }
+
+    @Test
+    public void createCliente_ComDadosInvalidos_RetornarErrorMessageComStatus422(){
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "jose@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("", ""))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "jose@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("José", "00000000000"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "jose@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("José", "688.270.970-07"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+    }
+
+    @Test
+    public void createCliente_ComUsuarioNaoPermitido_RetornarErrorMessageComStatus403(){
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("José Castro", "68827097007"))
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(403);
+    }
+}
---
+++ src/test/resources/sql/clientes/clientes-insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO USUARIOS (id, username, password, role) VALUES (100, 'admin@email.com', '$2a$10$xihFxLv3NqzQbLgwJc5FaOLhd7lkPi8pQHfMfQN8rMeMbspDK0BUi', 'ROLE_ADMIN');
+INSERT INTO USUARIOS (id, username, password, role) VALUES (101, 'maria@email.com', '$2a$10$xihFxLv3NqzQbLgwJc5FaOLhd7lkPi8pQHfMfQN8rMeMbspDK0BUi', 'ROLE_CLIENTE');
+INSERT INTO USUARIOS (id, username, password, role) VALUES (102, 'joao@email.com', '$2a$10$xihFxLv3NqzQbLgwJc5FaOLhd7lkPi8pQHfMfQN8rMeMbspDK0BUi', 'ROLE_CLIENTE');
+INSERT INTO USUARIOS (id, username, password, role) VALUES (103, 'jose@email.com', '$2a$10$xihFxLv3NqzQbLgwJc5FaOLhd7lkPi8pQHfMfQN8rMeMbspDK0BUi', 'ROLE_CLIENTE');
+
+INSERT INTO CLIENTES (id, nome, cpf, id_usuario) VALUES (10, 'Maria Santos', '38641465006', 101);
+INSERT INTO CLIENTES (id, nome, cpf, id_usuario) VALUES (20, 'João Silva', '81315448009', 102);
