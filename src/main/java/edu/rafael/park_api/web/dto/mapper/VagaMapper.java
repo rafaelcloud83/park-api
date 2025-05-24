f5652e994bcd19ce16da003d7cfe385c2a0d5de5
rafaelcloud83 <rafaelcloud83@gmail.com>
2025-05-23 21:42:57 -0300

    feat:criado DTOs e Mapper para Vaga

---
+++ src/main/java/edu/rafael/park_api/web/dto/mapper/VagaMapper.java
@@ -0,0 +1,20 @@
+package edu.rafael.park_api.web.dto.mapper;
+
+import edu.rafael.park_api.entity.Vaga;
+import edu.rafael.park_api.web.dto.VagaCreateDto;
+import edu.rafael.park_api.web.dto.VagaResponseDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class VagaMapper {
+
+    public static Vaga toVaga(VagaCreateDto dto) {
+        return new ModelMapper().map(dto, Vaga.class);
+    }
+
+    public static VagaResponseDto toDto(Vaga vaga) {
+        return new ModelMapper().map(vaga, VagaResponseDto.class);
+    }
+}
