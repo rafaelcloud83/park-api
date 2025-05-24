6e86f14be2adb4a8956e66f24408721c2b73d81e
rafaelcloud83 <rafaelcloud83@gmail.com>
2025-05-24 15:44:16 -0300

    docs:implementado documentação da classe de Vagas

--- src/main/java/edu/rafael/park_api/web/controller/VagaController.java
+++ src/main/java/edu/rafael/park_api/web/controller/VagaController.java
@@ -5,8 +5,17 @@ import edu.rafael.park_api.service.VagaService;
 import edu.rafael.park_api.web.dto.VagaCreateDto;
 import edu.rafael.park_api.web.dto.VagaResponseDto;
 import edu.rafael.park_api.web.dto.mapper.VagaMapper;
+import edu.rafael.park_api.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,6 +24,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
+@Tag(name = "Vagas", description = "Contém todas as operações relativas ao recurso de uma vaga")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/vagas")
@@ -22,6 +32,24 @@ public class VagaController {
 
     private final VagaService vagaService;
 
+    @Operation(
+            summary = "Criar uma nova vaga",
+            description = "Requisição exige um Bearer Token. Acesso restrito a ADMIN",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Vaga criada com sucesso",
+                            headers = @Header(name = HttpHeaders.LOCATION, description = "URL da vaga criada")),
+                    @ApiResponse(responseCode = "401", description = "Token inválido ou expirado",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "409", description = "Vaga já cadastrada",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Vaga com dados inválidos",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @PostMapping()
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> create(@RequestBody @Valid VagaCreateDto dto) {
@@ -34,6 +62,22 @@ public class VagaController {
         return ResponseEntity.created(location).build();
     }
 
+    @Operation(
+            summary = "Buscar uma vaga pelo código",
+            description = "Requisição exige um Bearer Token. Acesso restrito a ADMIN",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Vaga encontrada com sucesso",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = VagaResponseDto.class))),
+                    @ApiResponse(responseCode = "401", description = "Token inválido ou expirado",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Vaga não encontrada",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @GetMapping("/{codigo}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<VagaResponseDto> getByCode(@PathVariable String codigo) {
