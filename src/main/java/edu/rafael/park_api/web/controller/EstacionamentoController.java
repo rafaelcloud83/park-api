df3c299cf7061d516f93042f46af3aef6bb4fc70
rafaelcloud83 <rafaelcloud83@gmail.com>
2025-05-25 21:52:40 -0300

    docs:implementado documentação para método checkIn(entrada) de veículos

--- src/main/java/edu/rafael/park_api/web/controller/EstacionamentoController.java
+++ src/main/java/edu/rafael/park_api/web/controller/EstacionamentoController.java
@@ -5,8 +5,17 @@ import edu.rafael.park_api.service.EstacionamentoService;
 import edu.rafael.park_api.web.dto.EstacionamentoCreateDto;
 import edu.rafael.park_api.web.dto.EstacionamentoResponseDto;
 import edu.rafael.park_api.web.dto.mapper.ClienteVagaMapper;
+import edu.rafael.park_api.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +26,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
+@Tag(name = "Estacionamentos", description = "Contém todas as operações relativas ao recurso de entrada e saída de veículos de um estacionamento")
 @RestController
 @RequestMapping("api/v1/estacionamentos")
 @RequiredArgsConstructor
@@ -24,6 +34,31 @@ public class EstacionamentoController {
 
     private final EstacionamentoService estacionamentoService;
 
+    @Operation(
+            summary = "Operação de check-in de veículo",
+            description = "Requisição exige um Bearer Token. Acesso restrito a ADMIN",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Estacionamento criado com sucesso",
+                            headers = @Header(name = HttpHeaders.LOCATION, description = "URL do estacionamento criada"),
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = EstacionamentoResponseDto.class))),
+                    @ApiResponse(responseCode = "401", description = "Token inválido ou expirado",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Usuário sem permissão de acesso",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Causas possíveis: <br/>" +
+                            "- CPF do cliente não encontrado; <br/>" +
+                            "- Nenhuma vaga livre foi encontrada;",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Estacionamento com dados inválidos",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @PostMapping("/check-in")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<EstacionamentoResponseDto> checkIn(@RequestBody @Valid EstacionamentoCreateDto dto) {
