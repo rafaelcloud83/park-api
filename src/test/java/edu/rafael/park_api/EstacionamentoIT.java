6196331d471098a41f2601201719e423887afe75
rafaelcloud83 <rafaelcloud83@gmail.com>
2025-06-04 15:32:12 -0300

    test:implementado teste de ponta a ponta no método de busca de estacionamento pelo cliente logado

--- src/test/java/edu/rafael/park_api/EstacionamentoIT.java
+++ src/test/java/edu/rafael/park_api/EstacionamentoIT.java
@@ -295,4 +295,51 @@ public class EstacionamentoIT {
                 .jsonPath("path").isEqualTo("/api/v1/estacionamentos/cpf/81315448009")
                 .jsonPath("method").isEqualTo("GET");
     }
+
+    @Test
+    public void getAllEstacionamentosDoCliente_ComClienteLogado_RetornarDadosComStatus200() {
+        PageableDto responseBody = testClient
+                .get()
+                .uri("api/v1/estacionamentos/cliente?size=1&page=0")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "joao@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(PageableDto.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getContent().size()).isEqualTo(1);
+        Assertions.assertThat(responseBody.getTotalPages()).isEqualTo(2);
+        Assertions.assertThat(responseBody.getNumber()).isEqualTo(0);
+        Assertions.assertThat(responseBody.getSize()).isEqualTo(1);
+
+        responseBody = testClient
+                .get()
+                .uri("api/v1/estacionamentos/cliente?size=1&page=1")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "joao@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(PageableDto.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getContent().size()).isEqualTo(1);
+        Assertions.assertThat(responseBody.getTotalPages()).isEqualTo(2);
+        Assertions.assertThat(responseBody.getNumber()).isEqualTo(1);
+        Assertions.assertThat(responseBody.getSize()).isEqualTo(1);
+    }
+
+    @Test
+    public void getAllEstacionamentosDoCliente_ComPerfilAdmin_RetornarErrorMessageComStatus403() {
+        testClient
+                .get()
+                .uri("api/v1/estacionamentos/cliente")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "admin@email.com", "123456"))
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody()
+                .jsonPath("status").isEqualTo("403")
+                .jsonPath("path").isEqualTo("/api/v1/estacionamentos/cliente")
+                .jsonPath("method").isEqualTo("GET");
+    }
 }
